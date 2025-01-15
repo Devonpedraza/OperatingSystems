4a7bc09aae30cb6091441fce8becb4f89c4f815c
Devon Pedraza <94017563+Devonpedraza@users.noreply.github.com>
2025-01-15 15:01:45 -0500

    Add files via upload

---
+++ Exercises/Exercise3.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+
+    while (num > 0)
+    {
+        printf("%d\n", num);
+        num--;
+    }
+    printf("Happy New Year!\n");
+
+    return 0;
+}
